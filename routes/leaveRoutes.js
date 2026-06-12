9e5f95549a9e984f5becdf5a1af48db2f3b30e3c
Holly Zolly <test@example.com>
2026-06-12 18:42:44 +0530

    leave test route

--- routes/leaveRoutes.js
+++ routes/leaveRoutes.js
@@ -1,106 +1,3 @@
-// const express =
-//   require("express");
-
-// const router =
-//   express.Router();
-
-// const {
-//   applyLeave,
-//   teamLeadApproval,
-//   hrApproval,
-//   getMyLeaves,
-//   getLeaveBalance,
-//   createHoliday,
-//   getAllHolidays,
-//   getAllLeaves,
-// } = require(
-//   "../controllers/leaveController"
-// );
-
-// const {
-//   protect,
-// } = require(
-//   "../middleware/authMiddleware"
-// );
-
-// const {
-//   authorizeRoles,
-// } = require(
-//   "../middleware/roleMiddleware"
-// );
-
-// // ================= APPLY LEAVE =================
-// router.post(
-//   "/apply",
-//   protect,
-//   authorizeRoles(
-//     "employee"
-//   ),
-//   applyLeave
-// );
-
-// // ================= TEAM LEAD APPROVAL =================
-// router.put(
-//   "/teamlead-approval",
-//   protect,
-//   authorizeRoles(
-//     "teamlead"
-//   ),
-//   teamLeadApproval
-// );
-
-// // ================= HR APPROVAL =================
-// router.put(
-//   "/hr-approval",
-//   protect, 
-//   authorizeRoles(
-//     "hr",
-//     "admin"
-//   ),
-//   hrApproval
-// );
-
-// // ================= GET USER LEAVES =================
-// router.get(
-//   "/my-leaves/:userId",
-//   protect,
-//   getMyLeaves
-// );
-
-// // ================= GET LEAVE BALANCE =================
-// router.get(
-//   "/leave-balance/:userId",
-//   protect,
-//   getLeaveBalance
-// );
-
-// // ================= CREATE HOLIDAY =================
-// router.post(
-//   "/holiday/create",
-//   protect,
-//   authorizeRoles(
-//     "hr",
-//     "admin"
-//   ),
-//   createHoliday
-// );
-
-// // ================= GET ALL HOLIDAYS =================
-// router.get(
-//   "/holiday/all",
-//   protect,
-//   getAllHolidays
-// );
-
-// router.get(
-//   "/all",
-//   protect,
-//   getAllLeaves
-// );
-
-// module.exports =
-//   router;
-
 const express =
   require("express");
 
@@ -155,7 +52,7 @@ router.put(
 // ================= HR APPROVAL =================
 router.put(
   "/hr-approval",
-  protect,
+  protect, 
   authorizeRoles(
     "hr",
     "admin"
@@ -195,10 +92,9 @@ router.get(
   getAllHolidays
 );
 
-// ================= GET ALL LEAVES =================
-// Removed protect for testing in Postman
 router.get(
   "/all",
+  protect,
   getAllLeaves
 );
 
