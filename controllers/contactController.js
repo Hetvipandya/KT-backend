cf26977f0f92d04584c8083791e4bb5b236bdaa8
Holly Zolly <test@example.com>
2026-07-02 16:31:15 +0530

    Initial commit

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,4 +1,4 @@
-const Contact = require("../models/contact");
+const Contact = require("../models/Contact");
 
 // ================= CREATE CONTACT =================
 exports.createContact = async (req, res) => {
