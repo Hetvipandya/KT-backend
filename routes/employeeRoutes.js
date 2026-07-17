72785346617688b89dd884ab7fea67c3150652b9
Holly Zolly <test@example.com>
2026-07-17 17:04:12 +0530

    Initial commit

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Cloudinary Upload Middleware
-const upload = require("../middleware/upload");
+const upload = require("../middleware/uploadMiddleware");
 
 const {
   assignTeamLead,
